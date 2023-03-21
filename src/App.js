601b5f6f7792cb7e56b6a8cb6d7211f563b7978f
HahaKhoang <kathyhahoang@gmail.com>
2023-03-21 16:11:18 -0500

    Darth Vader Sound Board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [caption, setCaption] = useState("");
+
+  useEffect(() => {
+    document.addEventListener("keydown", (event) => {
+      if (drumPads.find((item) => item.text === event.key.toUpperCase())) {
+        soundHandler(event.key.toUpperCase());
+      }
+      return;
+    });
+  }, []);
+
+  const drumPads = [
+    {
+      keyCode: 81,
+      text: "Q",
+      caption: "Release your anger",
+      src: "https://www.mediacollege.com/downloads/sound-effects/star-wars/darthvader/darthvader_anger.wav",
+    },
+    {
+      keyCode: 87,
+      text: "W",
+      caption: "Don't underestimate the force",
+      src: "https://www.mediacollege.com/downloads/sound-effects/star-wars/darthvader/darthvader_dontunderestimate.wav",
+    },
+    {
+      keyCode: 69,
+      text: "E",
+      caption: "You have failed me for the last time",
+      src: "https://www.mediacollege.com/downloads/sound-effects/star-wars/darthvader/darthvader_failedme.wav",
+    },
+    {
+      keyCode: 65,
+      text: "A",
+      caption: "Give yourself to the dark side",
+      src: "https://www.mediacollege.com/downloads/sound-effects/star-wars/darthvader/darthvader_giveyourself.wav",
+    },
+    {
+      keyCode: 83,
+      text: "S",
+      caption: "I find your lack of faith disturbing",
+      src: "https://www.mediacollege.com/downloads/sound-effects/star-wars/darthvader/darthvader_lackoffaith.wav",
+    },
+    {
+      keyCode: 68,
+      text: "D",
+      caption: "It is pointless to resist, my son",
+      src: "https://www.mediacollege.com/downloads/sound-effects/star-wars/darthvader/darthvader_pointless.wav",
+    },
+    {
+      keyCode: 90,
+      text: "Z",
+      caption: "If you only knew the power of the dark side",
+      src: "https://www.mediacollege.com/downloads/sound-effects/star-wars/darthvader/darthvader_powerofthedarkside.wav",
+    },
+    {
+      keyCode: 88,
+      text: "X",
+      caption: "Don't make me destroy you",
+      src: "https://www.mediacollege.com/downloads/sound-effects/star-wars/darthvader/darthvader_dontmakeme.wav",
+    },
+    {
+      keyCode: 67,
+      text: "C",
+      caption: "I am your father",
+      src: "https://www.mediacollege.com/downloads/sound-effects/star-wars/darthvader/darthvader_yourfather.wav",
+    },
+  ];
+
+  const soundHandler = (id) => {
+    const audio = document.getElementById(id);
+    audio.play();
+
+    const letter = drumPads.find((item) => item.text === id);
+    const caption = letter.caption;
+
+    setCaption(caption);
+    console.log(id, caption);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <h1>There is no escape</h1>
+      <div id="drum-machine">
+        <div className="drum-pads">
+          <div className="left-controller">
+            <div className="left-panel"></div>
+            {drumPads.map((item) => (
+              <div
+                key={item.src}
+                onClick={() => {
+                  soundHandler(item.text);
+                }}
+                className={`drum-pad ${item.text}`}
+                id={item.src}
+              >
+                {item.text}
+                <audio className="clip" id={item.text} src={item.src}></audio>
+              </div>
+            ))}
+            <div className="right-panel"></div>
+          </div>
+          <div className="right-controller">
+            <div id="display">{caption}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
